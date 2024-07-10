8a65c3d07b6b2158a486f52198b3f52eaea732bb
Achu-Raj-Aswathy <achurajaswathy@gmail.com>
2024-07-11 02:42:02 +0530

    Student Portal angular

---
+++ src/app/student-add/student-add.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { AdminapiService } from '../services/adminapi.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-student-add',
+  templateUrl: './student-add.component.html',
+  styleUrls: ['./student-add.component.css']
+})
+export class StudentAddComponent {
+  constructor(private api:AdminapiService, private route:Router){}
+  student:any = {
+    id:"",
+    name:"",
+    email:"",
+    status:""
+  }
+  addstudent(){
+    console.log(this.student);
+    const {id, name, email, status} = this.student;
+    if(!id || !name || !email || !status){
+      Swal.fire({
+        title: "Oops...",
+        text: "Please fill the form completely",
+        icon: "info"
+      });
+    }
+    else{
+      this.api.addStudentApi(this.student).subscribe({
+        next:(res:any)=>{
+          console.log(res);
+          Swal.fire({
+            title: "Wow!",
+            text: "Student added successfully",
+            icon: "success"
+          });
+          // navigate to student list page
+          this.route.navigateByUrl('student')
+        },
+        error:(res:any)=>{
+          console.log(res);
+          Swal.fire({
+            title: "Oops...",
+            text: "Error in adding student",
+            icon: "info"
+          });
+        }
+      })
+    }
+  }
+  clearFields(){
+    this.student = {}
+  }
+}
